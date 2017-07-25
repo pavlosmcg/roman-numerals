917b814aa247d01532bc3ac798bedc9534d0fb45
pavlosmcg <pavlosmcg@users.noreply.github.com>
2017-07-25 20:41:01 +0100

    Introduce number construction logic - concatenate additions

--- Numerals.Tests/ConvertorTests.cs
+++ Numerals.Tests/ConvertorTests.cs
@@ -25,6 +25,10 @@ namespace Numerals.Tests
         [InlineData(2,"II")]
         [InlineData(3,"III")]
         [InlineData(4,"IV")]
+        [InlineData(5,"V")]
+        [InlineData(6,"VI")]
+        [InlineData(7,"VII")]
+        [InlineData(8,"VIII")]
         public void ToRomanNumerals_Returns_ExpectedResult(int input, string expected) {
             var unit = new Convertor();
 
--- Numerals/Convertor.cs
+++ Numerals/Convertor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Numerals 
 {
@@ -8,15 +9,28 @@ namespace Numerals
         private Dictionary<int, string> romanNumerals 
             = new Dictionary<int, string>{
                     {1,"I"},
-                    {2,"II"},
-                    {3,"III"},
-                    {4,"IV"}
-                };
+                    {4,"IV"},
+                    {5,"V"},
+                }.Reverse().ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
         public string ToRomanNumerals(int input) {
             if (input < 1 || input > 1000)
                 throw new ArgumentOutOfRangeException(); 
-            return romanNumerals[input];
+            
+            string numeral;
+            if (romanNumerals.TryGetValue(input, out numeral)){
+                return numeral;
+            }
+            
+            foreach(var item in romanNumerals) {
+                int value = item.Key;
+                if (value < input) {
+                    int remainder = input - value;
+                    return romanNumerals[value] + ToRomanNumerals(remainder);
+                }
+            }
+
+            return null;
         }
     }
 }
