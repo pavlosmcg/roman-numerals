1db857ec98fc9a5892684afc9908f82838fa2552
pavlosmcg <pavlosmcg@users.noreply.github.com>
2017-07-27 17:28:41 +0100

    Remove duplication of test data - use xunit 'ClassData' attribute to load data

--- Numerals.Tests/IntExtensionsTests.cs
+++ Numerals.Tests/IntExtensionsTests.cs
@@ -27,55 +27,8 @@ namespace RomanNumerals.Tests
         }
 
         [Theory]
-        [InlineData(1,"I")]
-        [InlineData(2,"II")]
-        [InlineData(3,"III")]
-        [InlineData(4,"IV")]
-        [InlineData(5,"V")]
-        [InlineData(6,"VI")]
-        [InlineData(7,"VII")]
-        [InlineData(8,"VIII")]
-        [InlineData(9,"IX")]
-        [InlineData(10,"X")]
-        public void ToRomanNumerals_Returns_ExpectedResultForNumbersUpTo10(int input, string expected) {
-            string result = input.ToRomanNumerals();
-
-            Assert.Equal(expected, result);
-        }
-
-        [Theory]
-        [InlineData(11,"XI")]
-        [InlineData(14,"XIV")]
-        [InlineData(18,"XVIII")]
-        [InlineData(21,"XXI")]
-        [InlineData(39,"XXXIX")]
-        [InlineData(41,"XLI")]
-        [InlineData(69,"LXIX")]
-        [InlineData(83,"LXXXIII")]
-        [InlineData(99,"XCIX")]
-        [InlineData(100,"C")]
-        public void ToRomanNumerals_Returns_ExpectedResultForNumbersUpTo100(int input, string expected) {
-            string result = input.ToRomanNumerals();
-
-            Assert.Equal(expected, result);
-        }
-
-        [Theory]
-        [InlineData(555,"DLV")]
-        [InlineData(679,"DCLXXIX")]
-        [InlineData(999,"CMXCIX")]
-        [InlineData(1001,"MI")]
-        [InlineData(1111,"MCXI")]
-        [InlineData(1453,"MCDLIII")]
-        [InlineData(1903,"MCMIII")]
-        [InlineData(1983,"MCMLXXXIII")]
-        [InlineData(2001,"MMI")]
-        [InlineData(2017,"MMXVII")]
-        [InlineData(3000,"MMM")]
-        [InlineData(3116,"MMMCXVI")]
-        [InlineData(3728,"MMMDCCXXVIII")]
-        [InlineData(3999,"MMMCMXCIX")]
-        public void ToRomanNumerals_Returns_ExpectedResultForNumbersUpTo3999(int input, string expected) {
+        [ClassData(typeof(TestData))]
+        public void ToRomanNumerals_Returns_ExpectedResultForNumbersUpTo3999(string expected, int input) {
             string result = input.ToRomanNumerals();
 
             Assert.Equal(expected, result);
